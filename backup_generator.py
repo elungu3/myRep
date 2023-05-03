4a14589697c534251fe39393ef8785c891e1e76c
elungu3 <95742506+elungu3@users.noreply.github.com>
2023-05-03 21:47:00 +0300

    Update backup_generator.py

--- backup_generator.py
+++ backup_generator.py
@@ -1,62 +1,136 @@
 import os
 import shutil
-from datetime import datetime
 import time
+import hashlib
+from datetime import datetime
 
 
-def backup_generator():
+class Log:
 
-    """
-    This function is generating a backup directory called 'Replica' with exactly match from the source dir.
-    There are no parameters, as the location of both dir (the source and replica) has to be given in the CLI.
-    The function also generates a log file which contains all the operations done and the time of them.
-    Synchronization interval should be given in terminal as an input (and) it is measured in minutes.
-    All the operations done are also printed in terminal.
-    """
+    def create_backup(self, logg_location):
+        log = open(logg_location + f"\log.txt", 'a')
+        log.write('The backup was created \n')
+        log.close()
 
-    source_location = input("Enter the absolute path of the file to backup: ")
-    backup_location = input("Enter the absolute path to the location where you want the backup: ")
-    log_location = input("Enter the absolute path to where you want the log file: ")
-    time_sync = int(input("Please specify the time (in minutes) you want to sync the file with the backup file: "))
+    def dir_added(self, filepath, logg_location):
+        log = open(logg_location + f"\log.txt", 'a')
+        log.write(f"{filepath} was created in the source, "
+                          f"adding it to backup also @ {datetime.now()} \n")
+        log.close()
 
-    while True:
-        source_b = os.listdir(source_location)
-        l_backup = []
-        l_source = []
-        deleted_elem = []
-        for k in source_b:
-            l_source.append(k)
-        if "Replica" in os.listdir(backup_location):
-            backup_b = os.listdir(f"{backup_location}\Replica")
-            for i in backup_b:
-                l_backup.append(i)
-            for each_file in backup_b:
-                if str(each_file) not in l_source:
-                    deleted_elem.append(each_file)
-                    _ = str(backup_location + r'\Replica' + f"\{each_file}")
-                    try:
-                        os.remove(path=_)
-                        print(f"{each_file} was deleted from the Replica directory "
-                                f"because it was no longer in the Source directory")
-                    except PermissionError or OSError:
-                        shutil.rmtree(path=_)
-                        print(f"{each_file} was deleted from the Replica directory "
-                              f"because it was no longer in the Source directory")
-        shutil.copytree(source_location, f"{backup_location}\Replica", dirs_exist_ok=True)
-        for each_file in source_b:
-            if each_file in l_backup:
-                print(f"{each_file} is already in that directory and it's now updated")
+    def file_added(self, filepath, logg_location):
+        log = open(logg_location + f"\log.txt", 'a')
+        log.write(f"{filepath} was created in the source, "
+                  f"adding it to backup also @ {datetime.now()} \n")
+        log.close()
+
+    def file_updated(self, filepath, logg_location):
+        log = open(logg_location + f"\log.txt", 'a')
+        log.write(f"{filepath} was updated in the source, "
+                  f"updating it to backup also @ {datetime.now()} \n")
+        log.close()
+
+    def deleted(self, filepath, logg_location):
+        log = open(logg_location + f"\log.txt", 'a')
+        log.write(f"{filepath} was deleted from the source, "
+                  f"deleting it from backup also @ {datetime.now()} \n")
+        log.close()
+
+
+writing_log = Log()
+
+source_location = input("Enter the absolute path of the file you want to create a backup to: ")
+backup_location = input("Enter the absolute path to the location where you want the backup: ")
+log_location = input("Enter the absolute path to where you want the log file: ")
+time_sync = int(input("Please specify the time (in minutes) you want to sync the file with the backup file: "))
+
+shutil.copytree(source_location, f"{backup_location}", dirs_exist_ok=True)
+log = open(log_location+f"\log.txt", 'a')
+log.write('The backup was created \n')
+log.close()
+
+
+def backup_generator(source_filepath: str, backup_filepath: str):
+    for i in os.listdir(source_filepath):
+        i_loc = str(source_filepath + f"\{i}")
+        if os.path.isdir(os.path.join(source_filepath, i)):
+            if not os.path.exists(os.path.join(backup_filepath, i)):
+                shutil.copytree(i_loc, os.path.join(backup_filepath, i))
+                writing_log.dir_added(os.path.join(source_filepath, i), log_location)
+                print(f"{i} was not in backup, added it")
             else:
-                print(f"{each_file} was added in the backup directory")
+                backup_generator(os.path.join(source_filepath, i), os.path.join(backup_filepath, i))
 
-        with open(f'{log_location}\log.txt', 'a') as f:
-            f.write(f"The backup directory is at the location: {backup_location} @ " + str(datetime.now()) + "\n")
-            for each_file in source_b:
-                if each_file in l_backup:
-                    f.write(f"{each_file} was updated in the backup directory \n")
-                else:
-                    f.write(f"{each_file} was added in the backup directory \n")
-            for j in deleted_elem:
-                f.write(f"Removing {j} from Replica as it was removed from Source directory \n")
-            f.write("\n")
-            time.sleep(time_sync*60)
+        elif os.path.isfile(os.path.join(source_filepath, i)):
+            if not os.path.exists(os.path.join(backup_filepath, i)):
+                try:
+                    shutil.copy2(i_loc, os.path.join(backup_filepath, i), follow_symlinks=False)
+                    print(f"{i} was not in backup, added it")
+                    writing_log.file_added(os.path.join(source_filepath, i), log_location)
+                except PermissionError:
+                    print("Permision err")
+            else:
+                hash_source = hashlib.md5(open(os.path.join(source_filepath, i), 'rb').read()).hexdigest()
+                hash_backup = hashlib.md5(open(os.path.join(backup_filepath, i), 'rb').read()).hexdigest()
+                if hash_backup != hash_source:
+                    shutil.copy2(i_loc, os.path.join(backup_filepath, i), follow_symlinks=False)
+                    print("hashul md5 nu era acelasi, asa ca am facut overwrite la fisier")
+                    writing_log.file_updated(os.path.join(source_filepath, i), log_location)
+
+
+
+def removing_files(source_filepath: str, backup_filepath: str):
+    for i in os.listdir(source_filepath):
+        if os.path.isdir(os.path.join(source_filepath, i)):
+            if not os.path.exists(os.path.join(backup_filepath, i)):
+                try:
+                    os.rmdir(path=os.path.join(source_filepath, i))
+                    writing_log.deleted(os.path.join(source_filepath, i), log_location)
+                    print(f"{os.path.join(source_filepath, i)} was deleted from the source, "
+                          f"deleting it from backup also")
+                except OSError:
+                    shutil.rmtree(path=os.path.join(source_filepath, i))
+                    writing_log.deleted(os.path.join(source_filepath, i), log_location)
+                    print(f"{os.path.join(source_filepath, i)} was deleted from the source, "
+                          f"deleting it from backup also")
+            else:
+                removing_files(os.path.join(source_filepath, i), os.path.join(backup_filepath, i))
+
+        elif os.path.isfile(os.path.join(source_filepath, i)):
+            if not os.path.exists(os.path.join(backup_filepath, i)):
+                os.remove(path=os.path.join(source_filepath, i))
+                writing_log.deleted(os.path.join(source_filepath, i), log_location)
+                print(f"{os.path.join(source_filepath, i)} was deleted from the source, deleting it from backup also")
+
+
+while True:
+    backup_generator(source_location, backup_location)
+    removing_files(backup_location, source_location)
+    time.sleep(time_sync*60)
+
+#Another implementation using os.walk
+
+# Source and Backup directories
+# source_dir = r'PATH'
+# backup_dir = r'PATH'
+#
+# while True:
+#     # Recursively traverse the Source directory
+#     for dirpath, dirnames, filenames in os.walk(source_dir):
+#         # Check if the current directory is in Backup
+#
+#
+#         if dirpath.replace(source_dir, backup_dir) != dirpath:
+#             # If not, create it in Backup
+#             new_dir = dirpath.replace(source_dir, backup_dir)
+#             os.makedirs(new_dir, exist_ok=True)
+#             print(f'Created {new_dir}')
+#
+#         # Copy any new files in the current directory to Backup
+#         for filename in filenames:
+#             source_file = os.path.join(dirpath, filename)
+#             backup_file = os.path.join(dirpath.replace(source_dir, backup_dir), filename)
+#             if not os.path.exists(backup_file):
+#                 shutil.copy2(source_file, backup_file)
+#                 print(f'Copied {source_file} to {backup_file}')
+#     time.sleep(30)
